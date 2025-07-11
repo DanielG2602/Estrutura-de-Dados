435c5fc16db0a4c38c66b7231a4523c29d1e60d0
DanielG2602 <daniel.comercial0981@.gmail.com>
2025-07-11 16:19:55 -0300

    Reverse BigO revisada

--- bigOrevisada.py
+++ bigOrevisada.py
@@ -6,5 +6,14 @@ def quicksort(array):
         menores = [x for x in array[1:] if x <= pivot]
         maiores = [x for x in array[1:] if x > pivot]
         return quicksort(menores) + [pivot] + quicksort(maiores)
-    
 print(quicksort([10, 5, 2, 3]))
+
+def quicksortReverse(array):
+    if len(array) <= 2:
+        return array
+    else:
+        pivot = array[0]
+        menores = [x for x in array[1:] if x >= pivot]
+        maiores = [x for x in array[1:] if x < pivot]
+        return quicksortReverse(menores) + [pivot] + quicksortReverse(maiores)
+print(quicksortReverse([10, 5, 2, 3]))
